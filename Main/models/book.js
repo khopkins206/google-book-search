25c25609b58fb2eac598fe978fdc6e46e83d537e
Katherine Jackson <katherine.jackson206@gmail.com>
2021-08-24 21:21:43 -0500

    added models

---
+++ Main/models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  googleId: Object,
+  title: Object,
+  subtitle: Object,
+  link: Object,
+  authors: Object,
+  description: Object,
+  image: Object,
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
