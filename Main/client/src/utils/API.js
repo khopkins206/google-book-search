0ff13f5bb45fb5871392eab8aacd124699fec3b6
Katherine Jackson <katherine.jackson206@gmail.com>
2021-08-24 21:21:53 -0500

    updated and deleted unnecessary files

---
+++ Main/client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default {
+  getSavedBooks: function () {
+    return axios.get("/api/books/");
+  },
+  getBooks: function (entry) {
+    console.log("Axios Get Books");
+    // console.log(params)
+    return axios.get("/api/google/", { params: { q: entry } });
+  },
+  saveBook: function (book) {
+    console.log("Axios Save");
+    console.log(book);
+    return axios.post("/api/books/", book);
+  },
+  deleteBook: function (id) {
+    console.log("Axios Delete");
+    console.log(id);
+    return axios.delete("/api/books/" + id);
+  },
+};
