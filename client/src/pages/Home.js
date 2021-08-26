ad669ef3d333c6e7b3911837056350cf4a6807bb
Katherine Jackson <katherine.jackson206@gmail.com>
2021-08-26 17:16:54 -0500

    fixed file paths

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
-import Form from "../../../components/Form";
-import Book from "../../../components/Book";
+import Form from "../components/Form";
+import Book from "../components/Book";
 import API from "../utils/API";
 import {
   Jumbotron,
